6bb8516987cd7eceae0d659417a9eb62e6436de5
nieyao <nieyao@wahaha.com.cn>
2021-05-11 08:51:40 +0800

    update

--- test.js
+++ test.js
@@ -1,33 +1,17 @@
-//[1,2,3]
-var permuteUnique = function (nums) {
-    const len = nums.length;
-    const res = [];
-    const circle = function (aimArr, leftArr) {
-
-        if (aimArr.length == len) {
-            res.push(aimArr);
-            return;
+//从最外层正方形开始旋转，每个位置向右边位移边长
+//不断递推里面正方形 直到没有 
+//原地 不能用额外的存储
+//从外层 每次递减两个循环次数logn 
+//逐层翻转
+//如果时逐层 一定可以用i j 代替 一定是个规律问题
+var rotate = function (matrix) {
+    let n = matrix.length
+    for (let i = 0; i < n - 1; i++)
+        for (let j = i; j < n - 1 - i; j++) {
+            let tmp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = tmp
         }
-        const useTop = [];
-        for (let i = 0; i < leftArr.length; i++) {
-            if (useTop.includes(leftArr[i])) {
-                continue;
-            }
-            //过滤重复选项
-            let lookdata = [...aimArr];
-            aimArr.push(leftArr[i]);
-            useTop.push(leftArr[i]);
-            let c = notInclude(leftArr, i);
-            circle(aimArr, c);
-            aimArr = [...lookdata];
-        }
-    }
-    circle([], nums);
-    return res;
 };
-var notInclude = function (arr, i) {
-    let c = [...arr];
-    c.splice(i, 1);
-    return c;
-}
-console.log(permuteUnique([1, 2, 3]));
