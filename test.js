1f71858963da5daa459010260651bcf43858febf
nieyao <nieyao@wahaha.com.cn>
2021-05-12 16:32:39 +0800

    加油第三天

--- test.js
+++ test.js
@@ -1,17 +1,11 @@
-/**
- * @param {string[]} strs
- * @return {string[][]}
- */
-//1、通过排序排序后可以确定两个为同母异位体
-var groupAnagrams = function (strs) {
-    const maparr = new Map();
-    for (let str of strs) {
-        const key = (Array.from(str).sort()).toString();
-        const list = maparr.get(key) ? maparr.get(key) : [];
-        list.push(str);
-        maparr.set(key, list);
+var findNumberIn2DArray = function (matrix, target) {
+    let a, b = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; i < matrix[i].length; j++) {
+            if (matrix[i][j] === target) {
+                return true;
+            }
+        }
     }
-    return Array.from(maparr.values());
+    return false;
 };
-
-console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
