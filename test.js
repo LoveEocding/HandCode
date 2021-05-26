c282d252eadf1f98fa5e08b1eb186c5001ec1e52
nieyao <1773734302@qq.com>
2021-05-26 20:36:00 +0800

    又是收货满满的一天

--- test.js
+++ test.js
@@ -1,6 +1,39 @@
-var isNumber = function(s) {
-   // return /^[+-]?(\d+(\.\d*)?|(\.\d+))(e[+-]?\d+)?$/i.test(s.trim())
-    const exp=/^[+-]?(\d+(\.\d*)?)(e[+-]?\d+)?$/ig;
-    return exp.test(s.trim());
-};
 
+//广度遍历，如果发现B的头相等的值，在进行这个树的广度遍历，如果一次对应没问题 
+function compare(a,b){
+    const queuea=[a];
+    const queueb=[b];
+    while(queueb.length){
+        const x=queuea.shift();
+        const y=queueb.shift();
+        if(!x||x.val!==y.val){
+            return false;
+        }
+
+        x.left&&queuea.push(x.left);
+        x.right&&queuea.push(x.right);
+        y.left&&queueb.push(y.left);
+        y.right&&queueb.push(y.right);
+    }
+    return true;
+}
+var isSubStructure = function(A, B) {
+     
+     const queue=[A];
+     while(queue.length){
+         const head=queue.shift();
+         if(head.val===B.val){
+             const res=compare(head,B);
+             if(res){
+                 return true;
+             }
+         }
+         head.left&&queue.push(head.left);
+         head.right&&queue.push(head.right);
+
+     }
+     return false;
+};
+const A={val:1,left:{val:2,left:null,right:null},right:{val:3,left:null,right:null}};
+const B={val:2,left:null,right:null};
+console.log(isSubStructure(A,B));
